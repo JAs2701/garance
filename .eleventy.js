03764dffcaf1ca69a001e07c157eb339bc7c613b
AQUINO SANCHEZ Jorge <jorge.aquinosanchez@ext.europarl.europa.eu>
2024-12-03 12:33:56 +0100

    update pageweb

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports=function(eleventyConfig) {
     return {
         dir: {
-            input: "src",
+            input: "src/_layout",
             output: "public",
         }
     }
